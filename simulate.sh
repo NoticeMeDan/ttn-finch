9aba5bf6450abd62924f4e5c0a48bb92e9c66924
Elias Jørgensen <eliaszakarias@gmail.com>
2020-02-24 17:16:40 +0100

    Create WIP device simulation script. Needs looping and documentation

---
+++ simulate.sh
@@ -0,0 +1,11 @@
+ttn-lw-cli simulate uplink \
+ --lorawan-version=1.1.0 \
+ --lorawan-phy-version=1.0.1 \
+ --gateway-id=test-gateway \
+ --dev-addr=0192EFE9 \
+ --f-nwk-s-int-key=739C5B160C869248F139B364BF1E9F7B \
+ --s-nwk-s-int-key=739C5B160C869248F139B364BF1E9F7B \
+ --nwk-s-enc-key=9619B7B3E16F72910D916623296D2EFA \
+ --app-s-key=E6A4F29F557A8A1F73BF763931D62433 \
+ --f-cnt=2 \
+ --frm-payload=1111
