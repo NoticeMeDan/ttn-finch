4bae245521748df4ba39763529dbc16e5388b41a
Elias Jørgensen <eliaszakarias@gmail.com>
2020-02-24 19:07:46 +0100

    Fix simulate file

--- simulate.sh
+++ simulate.sh
@@ -3,9 +3,9 @@ ttn-lw-cli simulate uplink \
  --lorawan-phy-version=1.0.1 \
  --gateway-id=test-gateway \
  --dev-addr=0192EFE9 \
- --f-nwk-s-int-key=739C5B160C869248F139B364BF1E9F7B \
+ --f-nwk-s-int-key=A1B02BD98BBB6175B7EE0901639002E3 \
  --s-nwk-s-int-key=739C5B160C869248F139B364BF1E9F7B \
  --nwk-s-enc-key=9619B7B3E16F72910D916623296D2EFA \
  --app-s-key=E6A4F29F557A8A1F73BF763931D62433 \
- --f-cnt=2 \
- --frm-payload=1111
+ --f-cnt=1 \
+ --frm-payload=48656A
