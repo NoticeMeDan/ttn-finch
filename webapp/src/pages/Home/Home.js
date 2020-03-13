2e6b2c2e8aeb176735f12d850a48790499cc03d7
IMarnild <im.arnild@gmail.com>
2020-03-13 16:12:21 +0100

    added button to create new flow

--- webapp/src/pages/Home/Home.js
+++ webapp/src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 function Home(props) {
 	return (
 		<div>
 			<h1>Så tager det fart!</h1>
+			<Button variant="contained" color="primary" href="/newflow">
+				Create New Flow
+      		</Button>
 		</div>
 	);
 }
