2a3c7dbcc64f6c49855dcc969159e90989e61a5e
IMarnild <im.arnild@gmail.com>
2020-03-22 20:57:11 +0100

    added flowoverview to home

--- webapp/src/pages/Home/Home.js
+++ webapp/src/pages/Home/Home.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Button } from "@material-ui/core";
+import FlowOverview from "./component/FlowOverview";
+import Grid from "@material-ui/core/Grid";
 
-function Home(props) {
+function Home() {
 	return (
-		<div>
+		<div style={{padding: 24}}>
 			<h1>Så tager det fart!</h1>
-			<Button variant="contained" color="primary" href="/newflow">
-				Create New Flow
-      		</Button>
+			<Grid container direction='column' spacing={2}>
+				<Grid container justify='flex-end'>
+					<Grid item>
+						<Button variant="contained" color="primary" href="/newflow">
+							Create New Flow
+						</Button>
+					</Grid>
+				</Grid>
+					<Grid item>
+						<FlowOverview/>
+					</Grid>
+			</Grid>
 		</div>
 	);
 }
