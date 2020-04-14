35df77996617f538021968c1b5f614eee72ae93c
IMarnild <im.arnild@gmail.com>
2020-04-14 15:41:53 +0200

    Added AppBar

--- webapp/src/pages/NewFlow/NewFlow.js
+++ webapp/src/pages/NewFlow/NewFlow.js
@@ -9,11 +9,6 @@ import useGetJson from '../../hooks/useGetJson'
 import Loading from '../../components/Loading'
 
 const useStyles = makeStyles({
-    root: {
-        height: '100vh',
-        backgroundColor: '#f0f0f0',
-        padding: '24px'
-    },
     header: {
         padding: 8,
         backgroundColor: '#f5f5f5'
@@ -51,7 +46,6 @@ function NewFlow () {
     }
 
     return (
-        <div className={classes.root}>
             <Paper>
                 <Paper className={classes.header}>
                     <Typography variant='h5' component='h5' color='primary' className={classes.title}>
@@ -64,7 +58,7 @@ function NewFlow () {
                         : <FlowForm handleSubmit={handleSubmit} handleCancel={history.goBack} results={results} />
                 }
             </Paper>
-        </div>)
+        )
 }
 
 export default NewFlow
