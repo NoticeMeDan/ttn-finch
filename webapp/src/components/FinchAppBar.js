5425a87da6ab742508df91a3d559f8d00b52ce8c
IMarnild <im.arnild@gmail.com>
2020-04-15 12:09:48 +0200

    link to home on appbar

--- webapp/src/components/FinchAppBar.js
+++ webapp/src/components/FinchAppBar.js
@@ -4,6 +4,8 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import logo from '../svg/Cardinal.svg'
+import {Link} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -26,15 +28,21 @@ function FinchAppBar () {
         <div className={classes.root}>
             <AppBar position='static' style={{ backgroundColor: '#f0f0f0' }}>
                 <Toolbar variant='regular'>
-                    <img src={logo} className={classes.logo} alt='logo' />
-                    <div>
-                        <Typography variant='subtitle2' color='textPrimary' className={classes.title}>
-                            THE THINGS
-                        </Typography>
-                        <Typography variant='h5' color='textPrimary' className={classes.title}>
-                            F I N C H
-                        </Typography>
-                    </div>
+                    <Link href={"/"} underline='none' style={{display: 'flex'}}>
+                            <img src={logo} className={classes.logo} alt='logo' />
+                            <Grid container direction='column' justify='center'>
+                                <Grid item>
+                                    <Typography display='block' variant='subtitle2' color='textPrimary' className={classes.title}>
+                                        THE THINGS
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Typography display='block' variant='h5' color='textPrimary' className={classes.title}>
+                                        F I N C H
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                    </Link>
                 </Toolbar>
             </AppBar>
         </div>
