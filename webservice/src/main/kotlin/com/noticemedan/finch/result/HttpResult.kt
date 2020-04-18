27c079b84fd5034ddb5a3ba7a91af0d5ec100aa5
Elias Jørgensen <ej@acto.dk>
2020-04-18 13:13:55 +0200

    Make results even easier to develop, and write the development guide

--- webservice/src/main/kotlin/com/noticemedan/finch/result/HttpResult.kt
+++ webservice/src/main/kotlin/com/noticemedan/finch/result/HttpResult.kt
@@ -6,7 +6,7 @@ import com.noticemedan.finch.dao.FlowDao
 import com.noticemedan.finch.dto.ResultDescription
 import com.noticemedan.finch.dto.ResultKind
 import com.noticemedan.finch.entity.Flow
-import com.noticemedan.finch.entity.ttn.QEventData
+import com.noticemedan.finch.entity.ttn.EventData
 import com.noticemedan.finch.result.schema.HttpSchema
 import com.noticemedan.finch.util.ActivityLogHelper
 import com.noticemedan.finch.util.DtoFactory
@@ -18,31 +18,19 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
-import java.time.Instant
 
 @Component
 class HttpResult(
-        private val flowDao: FlowDao,
-        private val eventDataDao: EventDataDao,
+        flowDao: FlowDao,
+        eventDataDao: EventDataDao,
         private val dtoFactory: DtoFactory,
         private val activityLogHelper: ActivityLogHelper,
         private val objectMapper: ObjectMapper,
         private val httpClient: OkHttpClient
-) : Result<HttpSchema>(flowDao, activityLogHelper, objectMapper, HttpSchema::class) {
+) : Result<HttpSchema>(flowDao, eventDataDao, activityLogHelper, objectMapper, HttpSchema::class) {
     @Transactional(propagation = Propagation.MANDATORY)
-    override fun process (flow: Flow, config: HttpSchema) {
-        val startedRunAt = Instant.now()
-
-        val query = QEventData.eventData.applicationId.eq(flow.applicationId).and(
-                QEventData.eventData.receivedAt.after(flow.resultConfig!!.lastRun))
-        val events = eventDataDao.findAll(query).toList()
-
-        if (events.isEmpty()) {
-            activityLogHelper.addLogLineToFlow("There are no new EventData, halting result", flow)
-            return
-        }
-
-        events.map(dtoFactory::toInfo).chunked(config.size).forEach {
+    override fun process (flow: Flow, config: HttpSchema, eventData: Set<EventData>) {
+        eventData.map(dtoFactory::toInfo).chunked(config.size).forEach {
             val body = objectMapper.writeValueAsString(it)
             val request = Request.Builder()
                     .url(config.url)
@@ -54,9 +42,6 @@ class HttpResult(
                     activityLogHelper.addLogLineToFlow("POST request failed with status code ${res.code}", flow)
             }
         }
-
-        flow.resultConfig!!.lastRun = startedRunAt
-        flowDao.save(flow)
     }
 
     override fun getDescription(): ResultDescription {
--- webservice/src/main/kotlin/com/noticemedan/finch/result/Result.kt
+++ webservice/src/main/kotlin/com/noticemedan/finch/result/Result.kt
@@ -1,31 +1,49 @@
 package com.noticemedan.finch.result
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.noticemedan.finch.dao.EventDataDao
 import com.noticemedan.finch.dao.FlowDao
 import com.noticemedan.finch.dto.ResultDescription
 import com.noticemedan.finch.entity.Flow
+import com.noticemedan.finch.entity.ttn.EventData
+import com.noticemedan.finch.entity.ttn.QEventData
 import com.noticemedan.finch.exception.FlowNotFound
 import com.noticemedan.finch.util.ActivityLogHelper
+import java.time.Instant
 import javax.transaction.Transactional
 import kotlin.reflect.KClass
 
 abstract class Result <T: Any> (
         private val flowDao: FlowDao,
+        private val eventDataDao: EventDataDao,
         private val activityLogHelper: ActivityLogHelper,
         private val objectMapper: ObjectMapper,
-        private val configClazz: KClass<T>
+        private val schemaClass: KClass<T>
 ) {
     @Transactional
     open fun run (flowId: Long) {
         val flow = flowDao.findById(flowId).orElseThrow { FlowNotFound() }
 
-        val config = objectMapper.treeToValue(flow.resultConfig!!.config, configClazz.java)
+        val config = objectMapper.treeToValue(flow.resultConfig!!.config, schemaClass.java)
 
         activityLogHelper.addLogLineToFlow("Running ${flow?.resultConfig?.kind} result", flow)
-        process(flow, config)
+
+        val query = QEventData.eventData.applicationId.eq(flow.applicationId).and(
+                QEventData.eventData.receivedAt.after(flow.resultConfig!!.lastRun))
+        val eventData = eventDataDao.findAll(query).toSet()
+
+        val startedRunAt = Instant.now()
+
+        if (eventData.isEmpty()) {
+            activityLogHelper.addLogLineToFlow("There are no new EventData, halting result", flow)
+        } else process(flow, config, eventData)
+
         activityLogHelper.addLogLineToFlow("${flow?.resultConfig?.kind} result finished", flow)
+
+        flow.resultConfig!!.lastRun = startedRunAt
+        flowDao.save(flow)
     }
 
-    abstract fun process (flow: Flow, config: T)
     abstract fun getDescription(): ResultDescription
+    abstract fun process (flow: Flow, config: T, eventData: Set<EventData>)
 }
