dc5ff2c1d123cdb9582333b080d20d53989ab6b3
Elias Jørgensen <ej@acto.dk>
2020-05-04 09:41:50 +0200

    Change ApplicationNotPartOfFlow exception HTTP status code

--- webservice/src/main/kotlin/com/noticemedan/finch/exception/ApplicationNotPartOfFlow.kt
+++ webservice/src/main/kotlin/com/noticemedan/finch/exception/ApplicationNotPartOfFlow.kt
@@ -3,5 +3,5 @@ package com.noticemedan.finch.exception
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.FORBIDDEN)
 class ApplicationNotPartOfFlow : RuntimeException()
